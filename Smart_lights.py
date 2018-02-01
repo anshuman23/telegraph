e5fbf4e7e4d321d24d18e863e0966f64c1f8bd5d
anirudh9654 <32608609+anirudh9654@users.noreply.github.com>
2018-02-01 20:22:36 +0530

    Add files via upload

---
+++ Smart_lights.py
@@ -0,0 +1,26 @@
+import random
+import time
+
+Lights=0
+motion_bool = random.choice([0,1]) #Will Be using Anshuman's Code Variable to Detect Motion
+
+Availability_of_daylight=random.randrange(0,100)
+Bluelight_content=random.randrange(0,100)
+Daylight_simulation=random.randrange(0,100)
+Dynamic_lighting=random.randrange(0,100)
+Tunable_white=random.randrange(0,100)
+illumination_of_room_surfaces=random.randrange(0,100)
+
+Luminosity=str(0.2*Availability_of_daylight+0.1*Bluelight_content+0.2*Daylight_simulation+0.2*Dynamic_lighting+0.2*Tunable_white+0.1*illumination_of_room_surfaces)
+
+
+if(motion_bool==0):
+	Lights=0
+else:
+	Lights=1
+	print "Current Brightness Set to "+ Luminosity+"%"
+		
+
+
+	
+	
