b3fc73130a41bfdb417c46bc504fdf93e05b0d48
anirudh9654 <32608609+anirudh9654@users.noreply.github.com>
2018-02-09 13:43:16 +0530

    Update Smoke_detector.py

--- Smoke_detector.py
+++ Smoke_detector.py
@@ -2,43 +2,42 @@ import random
 import time
 import threading
 
+#True for Lock and False for Open
 water_sprinkler=False
 option=[True,False]
 door=random.choice(option)
 windows=random.choice(option)
 
-def timer2(n):
+def timer(n):
 	print "Timer set for "+str(n)+" Minutes."
 	print "Please Evacuate!"
+	mins = 0
+		# Loop until we reach n minutes running
+	while mins != n:
+		# Sleep for a minute	
+		time.sleep(60)
+		# Increment the minute total
+		mins += 1
 	windows=True
 	door=True
-	
-	while n>0:
-		n=n-1
-		if(n==0):
-			water_sprinkler=True
-			break
-				
+	water_sprinkler=True
 
+i=1
 print "This is a trial Simulation of a Smoke Detector"
-while 1:
+while --i:
 	ppm=random.randint(1,500)
 	print ppm
 	if ppm<70:
+		timer(60)
 		water_sprinkler=False
 		continue
 	elif ppm>=70 and ppm<150:
-		timer = threading.Timer(60.0*60.0, timer2)
-		timer.start()
+		timer(10)		
 		break
 	elif ppm>=150 and ppm<400:
-		timer = threading.Timer(10.0*60.0, timer2)
-		timer.start()		
+		timer(4)
 		break
 	else:
-		timer = threading.Timer(4.0*60.0, timer2)
-		timer.start()
 		break
-
 
 
