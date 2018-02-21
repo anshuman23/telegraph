471ebba55eba41892c1f5bfd1b1fa9dc0e25eb69
anirudh9654 <32608609+anirudh9654@users.noreply.github.com>
2018-02-21 12:35:11 +0530

    Update Smart_Fridge.py

--- Smart_Fridge.py
+++ Smart_Fridge.py
@@ -1,4 +1,12 @@
 #Smart_Fridge
+import json
+import random
+import time
+import socket
+import os
+import csv
+
+
 '''
 Fridge is Divided into Different Zones
 1. The door:Warmest part of the fridge and subject to the most temperature fluctuations, so avoid storing highly perishable foods on the door
@@ -8,49 +16,85 @@ Fridge is Divided into Different Zones
 '''
 
 
-Temperature_of_Zones={'1':8,'2':5,'3':4,'4':6}
+Temperature_of_Zones={'1':6,'2':4,'3':2,'4':-2}
 
 def Water_Dispenser(Quantity):
 	print "Please Position your Bottle"
-	print "Filling "+Quantity+" Litres"
+	print "Filling "+str(Quantity)+" Litres"
 
 def Find_Zone(Find):
 	Zone={'Fruits':'Shelf'} #Will Add More Items Later
 
 	if(Zone.has_key(Find)==True):
 		print "Commodity Should be placed in "+Zone[Find]
+		return str(Zone[Find])
 	else:
 		print "Commodity Not Available in General List of Items!"
+		return "NA"
 
 def Adjust_Temperature(Zone,Temp):
 	Temperature_of_Zones[Zone]=Temp
-	print "Temperature adjusted to "+Temp+" degree celsius for Zone "+Zone	
+	print "Temperature adjusted to "+str(Temp)+" degree celsius for Zone "+str(Zone)	
 
 
-while 1:
-	print "\n"
-	print "Select Among the Options:"
-	print "1. Water Dispenser"
-	print "2. Smart Zone Finder"
-	print "3. Adjust Temperature"
-	print "4. Exit"
+
+def smart_fridge(room, ID):
+
+	start = time.time()
+	device = "smart_fridge"
+	ip = "127.0.0.1"
+	id = config[room][device]['id'][int(ID)]
+	p = int(config[room][device]['port'][int(ID)])
+
 	
 
-	choice=input()
-#print choice
+
+	while 1:
+		print "\n"
+		print "Select Among the Options:"
+		print "1. Water Dispenser"
+		print "2. Smart Zone Finder"
+		print "3. Adjust Temperature"
+		print "4. Exit"
+
+		present_time = round(time.time(), 5)
+		
 	
-	if (choice==1):
-		Quantity=raw_input("Enter Quantity in litres:")
-		Water_Dispenser(Quantity)
+		option=[1,2,3]
+		choice=3#random.choice(option)
+		#print choice
 
-	elif (choice==2):
-		Find=raw_input("Enter the Commodity for which you want to find the Appropriate Zone in the Refrigerator:")
-		Find_Zone(Find)	
+		if (choice==1):
 
-	elif(choice==3):
-		Zone=raw_input("Enter Zone(1/2/3/4):")
-		Temp=raw_input("Enter Temperature(Degree celsius):")
-		Adjust_Temperature(Zone,Temp)
-	else:
-		break
+			Quantity=random.randint(1,100)
+			Water_Dispenser(Quantity)
+			value='1:'+str(Quantity)			
 
+		elif (choice==2):
+			Find=raw_input("Enter the Commodity for which you want to find the Appropriate Zone in the Refrigerator:")
+			value='2:'+str(Find_Zone(Find))
+
+
+		elif(choice==3):
+				Zone=random.randint(1,4)
+				Temp=random.randint(-4,8)
+				Adjust_Temperature(Zone,Temp)
+				value='3:'+str(Zone)+str(Temp)
+
+		timeout = 1.0
+
+		message={}
+		message["id"] = str(id)
+		message["unix_timestamp"]=str(present_time)
+		message["data_size"]=str(len(str(value)))
+		message["data"]=str(value)
+
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		server__add = (ip, p)
+		sent = 0
+		message_to_send = str(message)
+		while(sent<len(message_to_send)):
+			sent = sent + sock.sendto(message_to_send[sent:len(message_to_send)], server__add)
+
+		csv.writer(open(DATA_DIR + id + '.csv', 'ab'), delimiter = ',').writerow([present_time, value])
+		time.sleep(timeout)
